f926fc83f2801bf020f02f44d97ef87493da8479
HyeongSeok Kim <tiryul@gmail.com>
2023-11-10 15:22:06 +0900

    update: process 생성 메서드를 ffmpeg 옵션에 더 직관적이게 수정

--- v2v/utils/_video_utils_.py
+++ v2v/utils/_video_utils_.py
@@ -1,5 +1,5 @@
 import subprocess
-from typing import Any
+from typing import Any, Dict, Literal, Union
 import numpy as np
 import ffmpeg
 
@@ -16,19 +16,47 @@ def read_frame_from_process(process: Any, width: int, height: int, channel: int 
     return frame
 
 
-def get_video_info_from_path(video_path: str):
+def get_video_info_from_path(video_path: str) -> dict:
     probe = ffmpeg.probe(video_path)
     video_info = next(s for s in probe["streams"] if s["codec_type"] == "video")
     return video_info
 
 
-def create_vi2_process(video_path: str):
-    args = (
-        ffmpeg.input(video_path)
-        .output("pipe:", format="rawvideo", pix_fmt="rgb24")
+def create_vi2_process(video_path: str) -> subprocess.Popen:
+    output_kwargs: Dict[str, str] = {
+        "format": "rawvideo",
+        "pix_fmt": "rgb24",
+    }
+    args = ffmpeg.input(video_path).output("pipe:", **output_kwargs).compile()
+    return subprocess.Popen(args, stdout=subprocess.PIPE)
+
+
+def create_i2v_process(
+    width: int,
+    height: int,
+    fps: Union[str, float, int],
+    pix_fmt: str,
+    video_bitrate: int,
+    format: Literal["h264", "h265"],
+) -> subprocess.Popen:
+    input_kwargs: Dict[str, str] = {
+        "format": "rawvideo",
+        "pix_fmt": "rgb24",
+        "s": f"{width}x{height}",
+        "framerate": str(fps),
+    }
+    output_kwargs: Dict[str, str] = {
+        "format": format,
+        "pix_fmt": pix_fmt,
+        "video_bitrate": video_bitrate,
+    }
+    pargs = (
+        ffmpeg.input("pipe:", **input_kwargs)
+        .output("pipe:", **output_kwargs)
+        .overwrite_output()
         .compile()
     )
-    return subprocess.Popen(args, stdout=subprocess.PIPE)
+    return subprocess.Popen(pargs, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
 
 __all__ = [
