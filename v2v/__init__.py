b90ff913d0c41e0e63be41af67ef8702d281a78e
HyeongSeok Kim <tiryul@gmail.com>
2023-11-13 13:06:53 +0900

    add: const 항목 추가

--- v2v/__init__.py
+++ v2v/__init__.py
@@ -2,3 +2,4 @@ from . import datastruct
 from . import utils
 from ._video2image_processor_ import *
 from ._audio_extractor_ import *
+from ._const_ import *
---
+++ v2v/_const_.py
@@ -0,0 +1,8 @@
+# https://ffmpeg.org/ffmpeg-codecs.html
+
+FFMPEG_PIX_FMT_BGR24 = "bgr24"
+FFMPEG_PIX_FMT_GRAY8 = "gray8"
+FFMPEG_PIX_FMT_RGB24 = "rgb24"
+FFMPEG_PIX_FMT_RGBA = "rgba"
+FFMPEG_PIX_FMT_YUV420P = "yuv420p"
+FFMPEG_PIX_FMT_YUYV422 = "yuyv422"
