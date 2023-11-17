ab575133fd5e4b3d48fa9969b269cd2361e1f380
HyeongSeok Kim <tiryul@gmail.com>
2023-11-17 10:42:27 +0900

    add: FrameProcessor & ProcessorPool interface 추가*
    - 이미지 처리용 프로세서를 넣기 위한 인터페이스

--- v2v/__init__.py
+++ v2v/__init__.py
@@ -5,4 +5,5 @@ from ._image2video_processor_ import *
 from ._audio_extractor_ import *
 from ._audio_merger_ import *
 from ._const_ import *
+from .interface import *
 from ._frame_organizer_ import *
