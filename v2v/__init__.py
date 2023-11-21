88d772c5ed079fefda5a1ed81ea9e278cc367abe
HyeongSeok Kim <tiryul@gmail.com>
2023-11-21 12:13:26 +0900

    update: init 리펙터링

--- v2v/__init__.py
+++ v2v/__init__.py
@@ -1,10 +1,11 @@
 """Python package for converting video to video with ffmpeg."""
 
 if "GITHUB_ACTION_TAG_NAME" in os.environ:
-  __version__ = os.environ["GITHUB_ACTION_TAG_NAME"]
+    __version__ = os.environ["GITHUB_ACTION_TAG_NAME"]
 else:
-  import datetime
-  __version__ = f"0.0.{datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))).strftime('%Y.%m.%d.%H.%M.%S')}"
+    import datetime
+
+    __version__ = f"0.0.{datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))).strftime('%Y.%m.%d.%H.%M.%S')}"
 
 from .datastruct import *
 from .utils import *
