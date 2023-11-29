9ad56299abe696d4d0365c7fbf505003d111172d
HyeongSeok <tiryul@gmail.com>
2023-11-29 08:47:03 +0000

    update: Video2Video 단순화*
    - 기존에 사용하던 Video2Image, Image2Video, AudioExtracter, AudioMerger를 모두 삭제
    - video_utils에서 create_v2v_processor를 가져와 직접 처리 및 병합 수행
    - 각 progress를 알 수 있게 property 추가

--- v2v/_video2video_processor_.py
+++ v2v/_video2video_processor_.py
@@ -3,57 +3,74 @@ from asyncio import Task
 from queue import Queue
 import functools
 import os
-from typing import Generator, Optional, Union
-from .datastruct import VideoInfo, FrameData
+from typing import Generator, Optional, Tuple, Union
+import threading
+import numpy as np
+from .datastruct import VideoInfo, FrameData, ThreadSafeProgress
 from .utils import (
     create_uuid,
+    create_v2v_process,
+    read_frame_from_process,
+    get_video_info_from_path,
 )
+import subprocess
 
 from . import (
-    Video2ImageProcessor,
-    Image2VideoProcessor,
-    AudioExtractor,
-    AudioMerger,
     IFrameProcessorPool,
     NullFrameProcessorPool,
 )
 
 
-def job_video2image(v2i_proc: Video2ImageProcessor, enqueue: Queue):
+def job_video2image(
+    v2i_proc: subprocess.Popen,
+    video_info: VideoInfo,
+    enqueue: Queue,
+    progress: ThreadSafeProgress,
+):
+    frame_width = video_info.frame_width
+    frame_height = video_info.frame_height
     while True:
-        frame_data = v2i_proc()
-        if frame_data is None:
+        frame = read_frame_from_process(
+            process=v2i_proc,
+            width=frame_width,
+            height=frame_height,
+        )
+        if frame is None:
             break
+        frame_data = FrameData(frame_id=progress.progress, frame=frame)
+        # print(f"v2i[{frame_data.frame_id:05d}]")
         enqueue.put(frame_data)
+        progress.change(1)
     enqueue.put(None)  # end of work
-
-
-def job_video2audio(v2a_proc: AudioExtractor):
-    v2a_proc.run()
+    v2i_proc.stdout.close()
+    v2i_proc.wait()
 
 
 async def job_image2image(
     i2i_pool: IFrameProcessorPool,
     dequeue: Queue,
     enqueue: Queue,
+    progress: ThreadSafeProgress,
 ):
     while True:
-        frame_data: FrameData = dequeue.get()
+        frame_data: FrameData = await asyncio.to_thread(dequeue.get)
         if frame_data is None:
             dequeue.put(None)
             break
         result = await i2i_pool(frame_data=frame_data)
+        # print(f"--------i2i[{frame_data.frame_id:05d}]")
         enqueue.put(result)
+        progress.change(1)
     enqueue.put(None)  # end of work
 
 
 def job_image2video(
-    i2v_proc: Image2VideoProcessor,
+    i2v_proc: subprocess.Popen,
     i2i_pool: IFrameProcessorPool,
     dequeue: Queue,
+    progress: ThreadSafeProgress,
 ):
     temp_state = {}
-    progress = 0
     n_worker = len(i2i_pool)
     while True:
         frame_data: FrameData = dequeue.get()
@@ -65,98 +82,82 @@ def job_image2video(
                 continue
         key = frame_data.frame_id
         temp_state[key] = frame_data
-        while progress in temp_state:
-            target_item = temp_state.pop(progress)
-            i2v_proc(frame_data=target_item)
-            progress += 1
-    i2v_proc(None)  # end of work
-
-
-def job_merge_video_and_audio(va2v_proc: AudioMerger):
-    va2v_proc.run()
+        while progress.progress in temp_state:
+            target_item: FrameData = temp_state.pop(progress.progress)
+            i2v_proc.stdin.write(target_item.frame.astype(np.uint8).tobytes())
+            progress.change(1)
+    # end of work
+    i2v_proc.stdin.close()
+    i2v_proc.wait()
 
 
 class Video2VideoProcessor:
     def __init__(
         self,
         input_video_path: str,
-        temp_file_dir: str,
         output_video_path: str,
-        v2i_queue_size: int = 60,
-        i2v_queue_size: int = 60,
         frame_processor_pool: IFrameProcessorPool = NullFrameProcessorPool(
             1, 0.0001, 0.01
         ),
-        v2i_ffmpeg_options_output: Optional[dict] = None,
-        v2a_ffmpeg_options_output: Optional[dict] = None,
-        i2v_width: Optional[int] = None,
-        i2v_height: Optional[int] = None,
+        i2v_frame_size: Optional[Tuple[int, int]] = None,
         i2v_fps: Optional[Union[str, float, int]] = None,
-        i2v_ffmpeg_options_input: Optional[dict] = None,
-        i2v_ffmpeg_options_output: Optional[dict] = None,
-        va2v_ffmpeg_options_output: Optional[dict] = None,
     ) -> None:
-        assert os.path.isdir(temp_file_dir)
         self._id = create_uuid()
         self._input_video_path = input_video_path
-        self._temp_file_dir = temp_file_dir
         self._output_video_path = output_video_path
-        self._v2i_queue_size = v2i_queue_size
-        self._i2v_queue_size = i2v_queue_size
 
-        self._v2ip = Video2ImageProcessor(
+        v2ip, i2vp = create_v2v_process(
             video_path=input_video_path,
-            ffmpeg_options_output=v2i_ffmpeg_options_output,
+            dst_video_path=output_video_path,
+            dst_fps=i2v_fps,
+            dst_frame_size=i2v_frame_size,
         )
-        i2v_width = i2v_width if i2v_width else self._v2ip.video_info.frame_width
-        i2v_height = i2v_height if i2v_height else self._v2ip.video_info.frame_height
-        i2v_fps = i2v_fps if i2v_fps else self._v2ip.video_info.avg_frame_rate
-        i2v_ffmpeg_options_input = i2v_ffmpeg_options_input
-        i2v_ffmpeg_options_output = i2v_ffmpeg_options_output
 
-        self._i2vp = Image2VideoProcessor(
-            dst_video_path=self.temp_video_path,
-            width=i2v_width,
-            height=i2v_height,
-            fps=i2v_fps,
-            ffmpeg_options_input=i2v_ffmpeg_options_input,
-            ffmpeg_options_output=i2v_ffmpeg_options_output,
-        )
-        self._v2ap = AudioExtractor(
-            video_path=self.input_video_path,
-            dst_audio_path=self.temp_audio_path,
-            ffmpeg_options_output=v2a_ffmpeg_options_output,
-        )
-        self._va2vp = AudioMerger(
-            video_path=self.temp_video_path,
-            audio_path=self.temp_audio_path,
-            dst_video_path=self.output_video_path,
-            ffmpeg_options_output=va2v_ffmpeg_options_output,
+        self._v2i_proc = v2ip
+        self._i2v_proc = i2vp
+        self._input_video_info = VideoInfo(
+            get_video_info_from_path(video_path=input_video_path)
         )
         self._frame_processor_pool = frame_processor_pool
         self._is_running = False
         self._main_task: Optional[Task] = None
+        self._v2i_progress = ThreadSafeProgress(
+            "v2i", max_progress=self._input_video_info.nb_frames
+        )
+        self._i2i_progress = ThreadSafeProgress(
+            "i2i", max_progress=self._input_video_info.nb_frames
+        )
+        self._i2v_progress = ThreadSafeProgress(
+            "i2v", max_progress=self._input_video_info.nb_frames
+        )
 
     async def _async_run(self):
         self._is_running = True
-        video2image_proc = self._v2ip
-        video2audio_proc = self._v2ap
-        image2video_proc = self._i2vp
+        video2image_proc = self._v2i_proc
         image2image_pool = self._frame_processor_pool
-        videoaudio2video_proc = self._va2vp
-        v2i_queue = Queue(self.v2i_queue_size)
-        i2i_queue = Queue(len(image2image_pool))
+        image2video_proc = self._i2v_proc
+        video_info = self._input_video_info
+        v2i_queue = Queue(len(image2image_pool))
+        i2v_queue = Queue(len(image2image_pool))
+        v2i_progress = self._v2i_progress
+        i2i_progress = self._i2i_progress
+        i2v_progress = self._i2v_progress
         await asyncio.gather(
             asyncio.to_thread(
                 job_video2image,
                 v2i_proc=video2image_proc,
+                video_info=video_info,
                 enqueue=v2i_queue,
+                progress=v2i_progress,
             ),
             *[
-                job_image2image(
-                    i2i_pool=image2image_pool,
-                    dequeue=v2i_queue,
-                    enqueue=i2i_queue,
+                asyncio.create_task(
+                    job_image2image(
+                        i2i_pool=image2image_pool,
+                        dequeue=v2i_queue,
+                        enqueue=i2v_queue,
+                        progress=i2i_progress,
+                    )
                 )
                 for _ in range(len(image2image_pool))
             ],
@@ -164,23 +165,10 @@ class Video2VideoProcessor:
                 job_image2video,
                 i2v_proc=image2video_proc,
                 i2i_pool=image2image_pool,
-                dequeue=i2i_queue,
-            ),
-            asyncio.to_thread(
-                job_video2audio,
-                v2a_proc=video2audio_proc,
+                dequeue=i2v_queue,
+                progress=i2v_progress,
             ),
         )
-
-        await asyncio.to_thread(
-            job_merge_video_and_audio,
-            va2v_proc=videoaudio2video_proc,
-        )
-
-        os.remove(video2audio_proc.dst_audio_path)
-        os.remove(image2video_proc.dst_video_path)
-        assert not os.path.exists(video2audio_proc.dst_audio_path)
-        assert not os.path.exists(image2video_proc.dst_video_path)
         self._is_running = False
 
     async def async_run(self):
@@ -203,34 +191,24 @@ class Video2VideoProcessor:
         return self._output_video_path
 
     @property
-    def i2v_processor(self) -> Image2VideoProcessor:
-        return self._v2ip
-
-    @property
-    def temp_file_dir(self) -> str:
-        return self._temp_file_dir
-
-    @property
-    def v2i_queue_size(self) -> str:
-        return self._v2i_queue_size
-
-    @property
-    def i2v_queue_size(self) -> str:
-        return self._i2v_queue_size
-
-    @property
-    def temp_video_path(self) -> str:
-        return os.path.join(
-            self.temp_file_dir, f"{self.id}.{self._output_video_path.split('.')[-1]}"
-        )
-
-    @property
-    def temp_audio_path(self) -> str:
-        return os.path.join(self.temp_file_dir, f"{self.id}.m4a")
-
-    @property
     def is_running(self) -> bool:
         return self._is_running
+
+    @property
+    def input_video_info(self) -> VideoInfo:
+        return self._input_video_info
+
+    @property
+    def v2i_progress(self) -> ThreadSafeProgress:
+        return self._v2i_progress
+
+    @property
+    def i2i_progress(self) -> ThreadSafeProgress:
+        return self._i2i_progress
+
+    @property
+    def i2v_progress(self) -> ThreadSafeProgress:
+        return self._i2v_progress
 
 
 __all__ = [Video2VideoProcessor.__name__]
